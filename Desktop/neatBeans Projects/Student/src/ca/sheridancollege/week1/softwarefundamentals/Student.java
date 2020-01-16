a6dd19eae573f6ea910b4d6dacf82fdbcadb2d24
Ostap2463 <ostap.mlnk@gmail.com>
2020-01-16 15:17:17 -0500

    third Commit

--- Desktop/neatBeans Projects/Student/src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ Desktop/neatBeans Projects/Student/src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -14,6 +14,15 @@ public class Student {
     private String name;
     private int id;
     private String program;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public int getId() {
         return id;
